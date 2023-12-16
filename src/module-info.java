b55d8bbfc0f58125f1779e72d2d1298e26e9488b
tinycodecrank <tinycodecrank@gmail.com>
2023-12-16 18:24:05 +0100

    add exports to module-info

--- src/module-info.java
+++ src/module-info.java
@@ -1,3 +1,10 @@
 module linear_algebra
 {
-	requires de.tinycodecrank.Functional;}
+	exports de.tinycodecrank.math.vector;
+	exports de.tinycodecrank.math.vector._double;
+	exports de.tinycodecrank.math.vector._float;
+	exports de.tinycodecrank.math.vector._int;
+	exports de.tinycodecrank.math.vector._long;
+	
+	requires de.tinycodecrank.Functional;
+}
